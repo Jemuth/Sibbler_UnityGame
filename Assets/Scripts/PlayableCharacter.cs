d6a3b06f80ba8a5a4ab480d7d27a5c3293bc25cd
Jemuth <juanjanag@gmail.com>
2023-06-14 15:38:10 -0400

    Added movement and movement animation scripts

--- Assets/Scripts/PlayableCharacter.cs
+++ Assets/Scripts/PlayableCharacter.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,20 +8,28 @@ public class PlayableCharacter : GameCharacter
     [SerializeField] private float playerRotationSpeed = 60;
     [SerializeField] private Animator animator;
     [SerializeField] private PlayableCharacterData m_data;
+
+    // Velocity and acceleration parameters
     float velocityZ = 0.0f;
     float velocityX = 0.0f;
     public float acceleration = 2f;
     public float deceleration = 2f;
     public float maxWalkVelocity = 0.5f;
-    public float maxRunVelocity;
-    float currentMaxVelocity;
+    public float maxRunVelocity = 2f;
+    int VelocityZHash;
+    int VelocityXHash;
 
+    // Run and stamina bar parameters
     public StaminaBar m_staminaStatus;
     public bool runEnabled;
+    [SerializeField] private float m_baseWalkSpeed = 0.5f;
+    [SerializeField] private float m_baseRunSpeed = 2f;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        VelocityZHash = Animator.StringToHash("Velocity Z");
+        VelocityXHash = Animator.StringToHash("Velocity X");
     }
     private void OnApplicationFocus(bool hasFocus)
     {
@@ -32,112 +41,191 @@ public class PlayableCharacter : GameCharacter
         return m_data.speedMultiplier;
     }
 
-    private void PlayerMovement()
+    public void PlayerMovement()
     {
-        // Inputs
-        bool forwardPressed = Input.GetKey(KeyCode.W);
-        bool backPressed = Input.GetKey(KeyCode.S);
-        bool leftPressed = Input.GetKey(KeyCode.A);
-        bool rightPressed = Input.GetKey(KeyCode.D);
-        bool runPressed = Input.GetKey(KeyCode.LeftShift);
+        var totalWalkSpeed = m_baseWalkSpeed * m_data.speedMultiplier * Time.deltaTime;
+        var totalRunSpeed = m_baseRunSpeed * m_data.speedMultiplier * Time.deltaTime;
 
-        // set current maxVelocity
-  
-        if(!backPressed && runPressed && m_staminaStatus.currentStamina > m_data.maxStamina * 5 / 100)
+        // Walking
+
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
-            currentMaxVelocity = maxRunVelocity;
-            maxRunVelocity = 2f;
-        } else
+            transform.position += transform.forward * totalWalkSpeed;
+        }
+        if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
         {
-            currentMaxVelocity = maxWalkVelocity;
-            maxRunVelocity = 0.5f;
+            transform.position -= transform.forward * totalWalkSpeed;
+        }
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.right * totalWalkSpeed;
+        }
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            transform.position -= transform.right * totalWalkSpeed;
+        }
+        // Walking
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.S))
+        {
+            transform.position += transform.forward * totalRunSpeed;
+        }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.W))
+        {
+            transform.position -= transform.forward * totalRunSpeed;
+        }
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.right * totalRunSpeed;
+        }
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.D))
+        {
+            transform.position -= transform.right * totalRunSpeed;
         }
 
-        // Decelarate low stamina
+    }
 
-        if(runEnabled == false && velocityZ > 0.5f)
-        {
-            velocityZ -= Time.deltaTime * acceleration;
-        }
-        if (runEnabled == false && velocityX > 0.5f)
-        {
-            velocityZ -= Time.deltaTime * acceleration;
-        }
-        if (runEnabled == false && velocityX < -0.5f)
-        {
-            velocityZ += Time.deltaTime * acceleration;
-        }
+    private void ChangeVelocity(bool forwardPressed, bool rightPressed, bool leftPressed, bool backPressed, bool runPressed, float currentMaxVelocity)
+    {
         // Increase velocity according to inputs
-
-        // Forward velocity
+        // Increase velocityZ going forward
         if (forwardPressed && velocityZ < currentMaxVelocity)
         {
             velocityZ += Time.deltaTime * acceleration;
         }
-        // Back Velocity
+        // Decrease velocityZ if forward is not pressed
+        if (!forwardPressed && velocityZ > 0.0f)
+        {
+            velocityZ -= Time.deltaTime * deceleration;
+        }
+        // Increase velocityZ going backwards
         if (backPressed && velocityZ > -currentMaxVelocity)
         {
             velocityZ -= Time.deltaTime * acceleration;
         }
-        // Left velocity
-        if (leftPressed && velocityX > -currentMaxVelocity)
+        // Decrease velocityZ if backrward is not pressed
+        if (!backPressed && velocityZ < 0.0f)
         {
-            velocityX -= Time.deltaTime * acceleration;
+            velocityZ += Time.deltaTime * deceleration;
         }
-        // Right velocity
+        // Increase velocityX going right
         if (rightPressed && velocityX < currentMaxVelocity)
         {
             velocityX += Time.deltaTime * acceleration;
         }
-        // Decrease velocityZ
-        if (!forwardPressed && !backPressed && velocityZ > 0.0f)
+        // Increase velocityX going left
+        if (leftPressed && velocityX > -currentMaxVelocity)
         {
-            velocityZ -= Time.deltaTime * deceleration;
-        }
-     
-        // Reset velocityZ
-        if (!forwardPressed && !backPressed && velocityZ < 0.0f)
-        {
-            velocityZ += Time.deltaTime * deceleration;
+            velocityX -= Time.deltaTime * acceleration;
         }
         // Increase velocityX if left is not pressed and velocityX < 0
-        if(!leftPressed && velocityX < 0.0f)
+        if (!leftPressed && velocityX < 0.0f)
         {
             velocityX += Time.deltaTime * deceleration;
         }
         // Decrease velocityX if right is not pressed and velocityX > 0
-        if(!rightPressed && velocityX > 0.0f)
+        if (!rightPressed && velocityX > 0.0f)
         {
             velocityX -= Time.deltaTime * deceleration;
         }
-        // reset velocityx
-        if(!leftPressed && !rightPressed && velocityZ != 0.0f &&(velocityX > -0.05f && velocityX < 0.05f))
-        { 
-            velocityX = 0.0f; 
+
+    }
+
+    private void LockOrResetVelocity(bool forwardPressed, bool rightPressed, bool leftPressed, bool backPressed, bool runPressed, float currentMaxVelocity)
+    {
+         // Reset velocities
+        // Reset velocityZ
+        if (!forwardPressed && !backPressed && velocityZ != 0.0f && (velocityZ > -0.05f && velocityZ < 0.05f))
+        {
+            velocityZ = 0.0f;
         }
-        // lock forward
+        // Reset velocityX
+        if (!leftPressed && !rightPressed && velocityZ != 0.0f && (velocityX > -0.05f && velocityX < 0.05f))
+        {
+            velocityX = 0.0f;
+        }
+
+        // Locking speeds acording to max velocity
+        // Lock forward speed
         if (forwardPressed && runPressed && velocityZ > currentMaxVelocity)
         {
             velocityZ = currentMaxVelocity;
         }
-        // decelerate to walk speed
+        // Decelerate to walk speed
         else if (forwardPressed && velocityZ > currentMaxVelocity)
         {
             velocityZ -= Time.deltaTime * deceleration;
-            // round to the currentmaxvelocity
-            if(velocityZ > currentMaxVelocity && velocityZ < (currentMaxVelocity + 0.05f))
+            // Round to the current max velocity if within offset
+            if (velocityZ > currentMaxVelocity && velocityZ < (currentMaxVelocity + 0.05f))
             {
                 velocityZ = currentMaxVelocity;
             }
         }
-        // round to the currentmaxvelocity
+        // Round to the current max velocity if within offset
         else if (forwardPressed && velocityZ < currentMaxVelocity && velocityZ > (currentMaxVelocity - 0.05f))
         {
             velocityZ = currentMaxVelocity;
         }
-
-        animator.SetFloat("Velocity Z", velocityZ);
-        animator.SetFloat("Velocity X", velocityX);
+        // Lock backward speed
+        if (backPressed && runPressed && velocityZ < -currentMaxVelocity)
+        {
+            velocityZ = -currentMaxVelocity;
+        }
+        // Decelerate to walk speed
+        else if (backPressed && velocityZ < -currentMaxVelocity)
+        {
+            velocityZ += Time.deltaTime * deceleration;
+            // Round to the current max velocity if within offset
+            if (velocityZ < -currentMaxVelocity && velocityZ > (-currentMaxVelocity - 0.05f))
+            {
+                velocityZ = -currentMaxVelocity;
+            }
+        }
+        // Round to the current max velocity if within offset
+        else if (backPressed && velocityZ > -currentMaxVelocity && velocityZ < (-currentMaxVelocity + 0.05f))
+        {
+            velocityZ = -currentMaxVelocity;
+        }
+        // Locking left speed
+        if (leftPressed && runPressed && velocityX < -currentMaxVelocity)
+        {
+            velocityX = -currentMaxVelocity;
+        }
+        // Decelerate to walk speed
+        else if (leftPressed && velocityX < -currentMaxVelocity)
+        {
+            velocityX += Time.deltaTime * deceleration;
+            // Round to the current max velocity if within offset
+            if (velocityX < -currentMaxVelocity && velocityX > (-currentMaxVelocity - 0.05f))
+            {
+                velocityX = -currentMaxVelocity;
+            }
+        }
+        // Round to the current max velocity if within offset
+        else if (leftPressed && velocityX > -currentMaxVelocity && velocityX < (-currentMaxVelocity + 0.05f))
+        {
+            velocityX = -currentMaxVelocity;
+        }
+        // Locking right speed
+        if (rightPressed && runPressed && velocityX > currentMaxVelocity)
+        {
+            velocityX = currentMaxVelocity;
+        }
+        // Decelerate to walk speed
+        else if (rightPressed && velocityX > currentMaxVelocity)
+        {
+            velocityX -= Time.deltaTime * deceleration;
+            // Round to the current max velocity if within offset
+            if (velocityX > currentMaxVelocity && velocityX < (currentMaxVelocity + 0.05f))
+            {
+                velocityX = currentMaxVelocity;
+            }
+        }
+        // Round to the current max velocity if within offset
+        else if (rightPressed && velocityX < currentMaxVelocity && velocityX > (currentMaxVelocity - 0.05f))
+        {
+            velocityX = currentMaxVelocity;
+        }
     }
     
     private Vector3 GetPlayerRotation()
@@ -164,7 +252,7 @@ public class PlayableCharacter : GameCharacter
     }
     private void UseStamina()
     {
-        if (Input.GetKey(KeyCode.LeftShift) && runEnabled == true && velocityZ > 0)
+        if (Input.GetKey(KeyCode.LeftShift) && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && runEnabled == true && velocityZ > 0)
         {
             
             StaminaBar.instance.UseStamina(2);
@@ -174,10 +262,21 @@ public class PlayableCharacter : GameCharacter
 
     void Update()
     {
+        bool forwardPressed = Input.GetKey(KeyCode.W);
+        bool backPressed = Input.GetKey(KeyCode.S);
+        bool leftPressed = Input.GetKey(KeyCode.A);
+        bool rightPressed = Input.GetKey(KeyCode.D);
+        bool runPressed = Input.GetKey(KeyCode.LeftShift);
+        float currentMaxVelocity = runPressed ? maxRunVelocity : maxWalkVelocity;
+        animator.SetFloat(VelocityZHash, velocityZ);
+        animator.SetFloat(VelocityXHash, velocityX);
+
         PlayerMovement();
+        ChangeVelocity(forwardPressed, rightPressed, leftPressed, backPressed, runPressed, currentMaxVelocity);
+        LockOrResetVelocity(forwardPressed, rightPressed, leftPressed, backPressed, runPressed, currentMaxVelocity);
         RotatePlayer(GetPlayerRotation());
-        UseStamina();
-        RunCooldown();
+        //UseStamina();
+        //RunCooldown();
 
     }
 }
