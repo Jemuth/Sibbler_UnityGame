c4010630d9eb6ace4746529801506a8f33fc2cdb
Jemuth <juanjanag@gmail.com>
2023-06-16 02:13:41 -0400

    Minor code fix

--- Assets/Scripts/CharacterSwap.cs
+++ Assets/Scripts/CharacterSwap.cs
@@ -8,14 +8,10 @@ public class CharacterSwap : MonoBehaviour
     public List<Transform> possibleCharacters;
     private bool p1Active = true;
     private bool canChange;
-    //[SerializeField] private CinemachineVirtualCamera cam1, cam2;
     private void Start()
     {
         EnablePlayer(possibleCharacters[0]);
         DisablePlayer(possibleCharacters[1]);
-       // camera1 = cam1.GetComponent<CinemachineVirtualCamera>();
-        //camera2 = cam2.GetComponent<CinemachineVirtualCamera>();
-        //camera1.SetActive(true);
     }
     public void ChangeEnabled(bool changeEnabled)
     {
@@ -30,16 +26,12 @@ public class CharacterSwap : MonoBehaviour
                 DisablePlayer(possibleCharacters[0]);
                 EnablePlayer(possibleCharacters[1]);
                 p1Active = false;
-               //camera1.SetActive(false);
-                //camera2.SetActive(true);
                 Debug.Log("Player 2 Active");
             } else
             {
                 DisablePlayer(possibleCharacters[1]);
                 EnablePlayer(possibleCharacters[0]);
                 p1Active=true;
-                //camera1.SetActive(true);
-                //camera2.SetActive(false);
                 Debug.Log("Player 1 Active");
             }
         }
