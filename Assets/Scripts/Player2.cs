8b99ff342ebaabaa07384284d649aa61d1e39109
Jemuth <juanjanag@gmail.com>
2023-06-24 17:26:03 -0400

    Added player 2 skill

--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -7,14 +7,22 @@ using UnityEngine.TextCore.Text;
 public class Player2 : PlayableCharacter
 {
     [SerializeField] private Animator m_crouchAnimation;
+    [SerializeField] private GameObject itemPrefab;
     private bool onCrouchZone;
+    private bool canUseSkill2;
+    private bool skillPressed2;
+    private void Start()
+    {
+        canUseSkill2 = true;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Tunnel"))
+        if (other.gameObject.CompareTag("Tunnel"))
         {
             onCrouchZone = true;
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Tunnel"))
@@ -22,6 +30,7 @@ public class Player2 : PlayableCharacter
             onCrouchZone = false;
         }
     }
+
     private void CrouchChecker()
     {
         if (onCrouchZone)
@@ -31,32 +40,97 @@ public class Player2 : PlayableCharacter
             runEnabled = false;
             maxRunSpeed = 0;
         }
-         else
+        else
         {
             m_crouchAnimation.SetBool("IsCrouching", false);
         }
-        
     }
+
     private void Crawling()
     {
-        if(onCrouchZone && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) && !Input.GetKey(KeyCode.LeftShift))
+        if (onCrouchZone && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) && !Input.GetKey(KeyCode.LeftShift))
         {
             m_crouchAnimation.SetBool("IsMovingCrouch", true);
-        } else
+        }
+        else
         {
             m_crouchAnimation.SetBool("IsMovingCrouch", false);
         }
-
     }
+
+    private void UseSkill2()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canUseSkill2)
+        {
+            skillPressed2 = true;
+            Debug.Log("Skill pressed");
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && !canUseSkill2)
+        {
+            // Debug.Log("Cannot use that now!");
+        }
+        else
+        {
+            skillPressed2 = false;
+        }
+    }
+
+    private IEnumerator WaitToMove()
+    {
+        canMove = false;
+        canRotate = false;
+        yield return new WaitForSeconds(1.5f);
+        canMove = true;
+        canRotate = true;
+    }
+
+    private IEnumerator AbilityCooldown()
+    {
+        canUseSkill2 = false;
+        yield return new WaitForSeconds(5);
+        canUseSkill2 = true;
+    }
+    private IEnumerator DropBearTimer()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Vector3 spawnPosition = transform.position + transform.forward * 50f; // Adjust the distance as per your requiremen
+        Instantiate(itemPrefab, transform.position, Quaternion.identity);
+    }
+    private void DestroyAllBears()
+    {
+        GameObject[] bears = GameObject.FindGameObjectsWithTag("Bear");
+
+        foreach (GameObject bear in bears)
+        {
+            Destroy(bear);
+        }
+    }
+    private IEnumerator DestroyAllBearsCoroutine()
+    {
+        yield return new WaitForSeconds(5);
+        DestroyAllBears();
+    }
+
+    private void DropBear()
+    {
+        if (canUseSkill2 && skillPressed2 && !onCrouchZone)
+        {
+            m_crouchAnimation.SetBool("IsDrop", true);
+            StartCoroutine(DropBearTimer());
+            StartCoroutine(AbilityCooldown());
+            StartCoroutine(WaitToMove());
+            StartCoroutine(DestroyAllBearsCoroutine());
+        } else
+        {
+            m_crouchAnimation.SetBool("IsDrop", false);
+        }
+    }
+
     protected override void OnUpdating()
     {
         CrouchChecker();
         Crawling();
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Debug.Log("P2 Skill");
-        }
+        DropBear();
+        UseSkill2();
     }
-
-
 }
